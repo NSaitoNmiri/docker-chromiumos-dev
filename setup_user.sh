816e60a72e34eb4bb1399e57a49eabe9431590e1
Naoki Saito <saito.naoki@nmiri.city.nagoya.jp>
2016-08-30 17:38:45 +0900

    fix #5. add script to install depot_tools into setup_user.sh

--- setup_user.sh
+++ setup_user.sh
@@ -1,2 +1,12 @@
 #!/usr/bin/env bash
 set -eu
+
+WORKDIR="$HOME/work/chromiumos_dev"
+
+# install depot_tools
+mkdir -p $WORKDIR
+cd $WORKDIR
+git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
+echo ""
+echo "# depot_tools" >> $HOME/.profile
+echo "export PATH=\$WORKDIR/depot_tools:\$PATH" >> $HOME/.profile
